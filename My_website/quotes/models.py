5a4c2ba1e844a6f2b3ebe33db6ee2c3a591557dd
Saw535 <130400768+Saw535@users.noreply.github.com>
2023-10-03 10:18:50 +0300

    Create models.py

---
+++ My_website/quotes/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Quote(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    text = models.TextField()
+
+    def __str__(self):
+        return self.text
+    
+
+class Meta:
+        managed = False
+        db_table = 'quotes_quote'
+        app_label = 'quotes'
+        using = 'mongodb'
